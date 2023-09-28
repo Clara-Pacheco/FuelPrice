f2860a4e0b73d7679a10a4df8e00957403f9b233
Clara <clarapachecodev@gmail.com>
2023-09-28 10:09:28 -0300

    Create Title styled component

---
+++ src/components/Fuel/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+
+export const Title = styled.h1`
+  font-family: Roboto;
+  font-weight: bold;
+
+  font-size: 48px;
+
+  color: ${(props) => props.theme.colorGrey200};
+
+  padding-top: 10px;
+
+
+
+
+
+`
