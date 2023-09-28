a917e5e224a50dfe7d00f5396af8564ce4706e98
Clara <clarapachecodev@gmail.com>
2023-09-28 09:35:56 -0300

    Create theme object containing colors for the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { FuelPage } from './pages/Fuel'
+import { ThemeProvider } from 'styled-components'
+import { theme } from './theme/theme'
 
 function App() {
  
   return (
-    <>
+    <ThemeProvider theme = {theme}>
       <FuelPage />
-    </>
+    </ThemeProvider>
   )
 }
 
