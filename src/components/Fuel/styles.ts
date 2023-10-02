78e412e0fc99fb66a583266ef9f2455594f89bb3
Clara <clarapachecodev@gmail.com>
2023-10-02 09:09:07 -0300

    Create an icon styled-component to add stylization for icon from react-icons

--- src/components/Fuel/styles.ts
+++ src/components/Fuel/styles.ts
@@ -2,12 +2,16 @@ import styled from "styled-components";
 
 import imageGasStation from '../../assets/gas-station.jpg'
 
+import { FiSettings } from 'react-icons/fi'
+
 export const Container = styled.div`
   background: url(${imageGasStation});
   background-size: cover;
   background-repeat: no-repeat;
   background-position: bottom;
   height: 100vh;
+
+  text-align: center;
 `
 
 export const Title = styled.h1`
@@ -20,7 +24,16 @@ export const Title = styled.h1`
 
   padding-top: 10px;
 
+`
 
+export const SettingIcon = styled(FiSettings) `
+  color: ${({ theme }) => theme.colorGray200};
+  font-size: 32px;
+  cursor: pointer;
+
+  position: absolute;
+  right: 20px;
+  top: 20px;
 
 
 
