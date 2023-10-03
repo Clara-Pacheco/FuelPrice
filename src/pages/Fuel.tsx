915f2ac5c1cfba5a873f2ce89ad37cc83451de32
Clara <clarapachecodev@gmail.com>
2023-10-03 16:55:17 -0300

    Create control to change the state between edit-mode and view mode and sent it as props to FuelComponent

--- src/pages/Fuel.tsx
+++ src/pages/Fuel.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react"
 import { FuelComponent } from "../components/Fuel"
 
 export const FuelPage = () => {
+
+  const [editMode, setEditMode] = useState(false)
+
+  function toggleEditMode () {
+    setEditMode(prev => !prev)
+  }
   return (
     <div>
-      <FuelComponent />
+      <FuelComponent editMode = {editMode} toggleEditMode = {toggleEditMode} />
     </div>
   )
 }
