f40078f3badf18a7243bbe517f1a393c27d83a83
Clara <clarapachecodev@gmail.com>
2023-10-03 11:09:32 -0300

    Create state variables and function to control and render changes, fetch and update data with useEffect and render conditionally using the data from api and array method map

--- src/components/Fuel/index.tsx
+++ src/components/Fuel/index.tsx
@@ -1,46 +1,41 @@
 
+import { useEffect, useState } from "react"
 import { getFuel } from "./services"
 import { Container, Panel, SettingIcon, Title, Box, Row, FuelText, FuelPrice } from "./styles"
+import { IFuel } from "./types"
 
 export const FuelComponent = () => {
-  getFuel()
+  const[fuels,setFuel] = useState<IFuel[]>()
+
+  async function fetchAndUpdateData () {
+    const data = await getFuel()
+    setFuel(data)
+  }
+
+  useEffect(() => {
+    fetchAndUpdateData();
+  }, [])
+  
   return (
     <Container>
       <Title>Posto ReactJS</Title>
       <SettingIcon />
       <Panel>
-        <Row>
-          <Box>
-            <FuelText>Gasolina Comum</FuelText>
-          </Box>
-          <Box>7
-            <FuelPrice>7,19</FuelPrice>
-          </Box>
-        </Row>
-        <Row>
-          <Box>
-            <FuelText>Etanol</FuelText>
-          </Box>
-          <Box>7
-            <FuelPrice>5,89</FuelPrice>
-          </Box>
-        </Row>
-        <Row>
-          <Box>
-            <FuelText>Diesel</FuelText>
-          </Box>
-          <Box>7
-            <FuelPrice>4,99</FuelPrice>
-          </Box>
-        </Row>
-        <Row>
-          <Box>
-            <FuelText>Diesel S10</FuelText>
-          </Box>
-          <Box>7
-            <FuelPrice>5,19</FuelPrice>
-          </Box>
-        </Row>
+       {fuels?.map((fuel)=>{
+         return (
+          <>
+          <Row key={fuel.id}>
+            <Box>
+              <FuelText>{fuel.name}</FuelText>
+            </Box>
+            <Box>7
+              <FuelPrice>{fuel.price}</FuelPrice>
+            </Box>
+          </Row>  
+          </>
+         )
+       })}
+       
       </Panel>
     </Container>
   )
